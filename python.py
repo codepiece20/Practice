e76963f798edd5460aa1088c8114583782536490
codepiece20 <codepiece20@gmail.com>
2024-06-22 17:04:19 +0500

    This is Version 2

--- python.py
+++ python.py
@@ -1,2 +1,10 @@
-name = input("What's Your Name? ")
-print(f"Hello {name}!")
+n = int(input("Number: "))
+
+if n > 0:
+    print(f"{n} is Positive!")
+
+elif n < 0:
+    print(f"{n} is Negative!")
+    
+else:
+    print(f"{n} is Zero!")
